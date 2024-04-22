4d842e9e5de7febfde96b86c8810691256a0dfa6
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2024-04-22 19:18:23 +0800

    完成多页面

--- AXmlToSvg/MainWindow.xaml.cs
+++ AXmlToSvg/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
+﻿using AXmlToSvg.Pages;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AXmlToSvg
 {
@@ -19,6 +11,36 @@ namespace AXmlToSvg
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void SingleFileModeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (PageFrame.Content is not SingleFile)
+            {
+                PageFrame.Content = null;
+                PageFrame.Content = LoadSingleFilePage();
+            }
+        }
+
+        private void MultiFileModeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (PageFrame.Content is not MultiFile)
+            {
+                PageFrame.Content = null;
+                PageFrame.Content = LoadMultiFilePage();
+            }
+        }
+
+        private static SingleFile LoadSingleFilePage()
+        {
+            SingleFile singleFile = new();
+            return singleFile;
+        }
+
+        private static MultiFile LoadMultiFilePage()
+        {
+            MultiFile multiFile = new();
+            return multiFile;
         }
     }
 }
