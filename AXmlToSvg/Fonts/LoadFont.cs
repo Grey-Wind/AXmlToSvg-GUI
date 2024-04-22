cadd92b2f0963f5d096ec9ecd5565d319cf36816
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2024-04-22 20:24:02 +0800

    Create LoadFont.cs

---
+++ AXmlToSvg/Fonts/LoadFont.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Media;
+
+namespace AXmlToSvg.Fonts
+{
+    internal class LoadFont
+    {
+        /// <summary>
+        /// 字体加载
+        /// </summary>
+        /// <returns>字体</returns>
+        public static FontFamily LXGWWenKai()
+        {
+            string LXGWWenKai = "pack://application:,,,/AXmlToSvg;component/Fonts/LXGWWenKai-Regular.ttf";
+
+            FontFamily customFont = new(new Uri(LXGWWenKai), "./#LXGWWenKai-Regular.ttf");
+
+            return customFont;
+        }
+
+        public static FontFamily YouSheTitleBlack()
+        {
+            string YouSheTitleBlack = "pack://application:,,,/AXmlToSvg;component/Fonts/YouSheTitleBlack.ttf";
+
+            FontFamily customFont = new(new Uri(YouSheTitleBlack), "./#YouSheTitleBlack.ttf");
+
+            return customFont;
+        }
+    }
+}
